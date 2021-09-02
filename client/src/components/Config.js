87b7c206d21a09256e97742efebf48139f2b7d22
Kukjin Lee <75208878+cptkuk91@users.noreply.github.com>
2021-09-02 23:02:10 +0900

    Try to Learn from Inflearn

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '7bea5c1a74f7463a23e404e3198e84b6';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
