e4b4932943707fb6f283216be8b95bd2774876b7
이국진 <cptkuk91@gmail.com>
2021-11-25 22:57:31 +0900

    Study Until Page 90

--- web1_html_internet-master 3/main.js
+++ web1_html_internet-master 3/main.js
@@ -1,22 +1,44 @@
-let http = require('http');
-let fs = require('fs');
-var url = require('url');
+let http = require("http");
+let fs = require("fs");
+var url = require("url");
 
-let app = http.createServer(function(request, response){
-    let _url = request.url;
-    let queryData = url.parse(_url, true).query;
-    console.log(queryData.id);
+let app = http.createServer(function (request, response) {
+  let _url = request.url;
+  let queryData = url.parse(_url, true).query;
+  console.log(queryData.id);
+  let title = queryData.id;
 
-    if(_url === '/'){
-        _url = '/index.html';
-    }
-    if(_url === '/favicon.ico'){
-        return response.writeHead(404);
-    }
-    response.writeHead(200);
-    console.log(__dirname + _url);
-    // response.end(fs.readFileSync(__dirname + _url));
-    response.end(queryData.id);
+  if (_url === "/") {
+    title = "Welcome";
+  }
+  if (_url === "/favicon.ico") {
+    return response.writeHead(404);
+  }
+  response.writeHead(200);
+  fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+    let template = `
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+            </head>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JAVASCRIPT</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>
+                ${description}
+            </p>
+        </body>
+        </html>
+    `;
+    response.end(template);
+  });
 });
 
 app.listen(3000);
