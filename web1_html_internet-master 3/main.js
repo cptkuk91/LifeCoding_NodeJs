66c8475d3bc8e3ff416cd8f2e619888eab57c5d6
이국진 <cptkuk91@gmail.com>
2021-11-25 22:01:10 +0900

    Study until Page 72

--- web1_html_internet-master 3/main.js
+++ web1_html_internet-master 3/main.js
@@ -1,15 +1,22 @@
 let http = require('http');
 let fs = require('fs');
+var url = require('url');
+
 let app = http.createServer(function(request, response){
-    let url = request.url;
-    if(request.url === '/'){
-        url = '/index.html';
+    let _url = request.url;
+    let queryData = url.parse(_url, true).query;
+    console.log(queryData.id);
+
+    if(_url === '/'){
+        _url = '/index.html';
     }
-    if(request.url === '/favicon.ico'){
+    if(_url === '/favicon.ico'){
         return response.writeHead(404);
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    console.log(__dirname + _url);
+    // response.end(fs.readFileSync(__dirname + _url));
+    response.end(queryData.id);
 });
 
 app.listen(3000);
