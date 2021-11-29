9459c1ff2245927ce4bb157c8cacf90686516cfc
이국진 <cptkuk91@gmail.com>
2021-11-29 21:26:40 +0900

    Study Page Until 130

---
+++ web1_html_internet-master 3/syntax/array.js
@@ -0,0 +1,10 @@
+let arr = ['a', 'b', 'c', 'd'];
+console.log(arr);
+console.log(arr[1]);
+console.log(arr[3]);
+arr[2] = 3;
+console.clear();
+console.log(arr);
+console.log(arr.length);
+arr.push('E');
+console.log(arr);
